20298dc33a76a1e5a5f0c8ee266d819d1608d223
Alex Fritz <alexfritzcoach@gmail.comm>
2021-12-22 11:38:51 -0800

    Complete activity

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let theFacts = [
+const theFacts = [
   'Most american kitchens are 110sqft.',
   'The average kitchen contains 2.7 spatulas.',
   "More water is run through colanders every year than goes over 2 Niagara Falls!",
@@ -7,55 +7,52 @@ let theFacts = [
   'The FDA recommends periodically rotating your knives, forks, and spoons (take the ones on the bottom, put them on top) to ensure even wear and tear and to extend the lifespan of your utensils.',
   "The Safe Operating Temperature Range for Pyrex is -32 to 487 degrees, but some villages in iceland have been known to use them to bake lasagna inside a volcano!",
   'The Guinness World Record for tallest stack of plates is 382 feet!', "Nothing edible has been discovered in nature since 1854 when the Royal Culinary Society discovered that the Greater Amazonian Soft Potato could be rendered palatable by a complicated process involving boiling and a three day curing process."
-
 ];
 
-var images = [
-  'kitchen.jpg', 'kitchen2.jpg', 'kitchen3.jpg', 'kitchen4.jpg', 'kitchen5.jpg', 'kitchen6.jpg', 'kitchen7.jpg',
-
+const images = [
+  'kitchen.jpg', 'kitchen2.jpg', 
+  'kitchen3.jpg', 'kitchen4.jpg', 
+  'kitchen5.jpg', 'kitchen6.jpg', 
+  'kitchen7.jpg',
 ];
 
-let emojis = ['🍇', '🍱','🍴', '🥢',
-'🥤'];
+const emojis = ['🍇', '🍱','🍴', '🥢', '🥤'];
 
+const body = document.querySelector('body');
+const button = document.getElementById('getFact');
+const email = document.getElementById('email');
+const nextFact = document.getElementById('nextFact');
+const factBox = document.getElementById('factBox');
 
-function fact() {
-
-
-  return theFacts[ Math.floor(Math.random() * theFacts.length)];
+const getRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length);
 }
 
-function emojii() {
-  return emojis[Math.floor(Math.random() * emojis.length)];
+const getRandom = (item) => {
+  return item[getRandomIndex(item)];
 }
 
-
-let button = document.querySelector('.clickToFact');
-
-const getTheBackgroundImage = () => {
-  return `url(./images/${images[Math.floor(Math.random() * images.length)]})`
-
+const getRandomBackgroundImage = () => {
+  return `url(./images/${images[getRandomIndex(images)]})`;
 }
 
-window.addEventListener('load', function() {
-  document.querySelector('body').style.backgroundImage = getTheBackgroundImage();
+const removeClass = (elements) => {
+    elements.forEach(item => item.classList.remove('hidden'));
+}
+
+window.addEventListener('load', () => {
+  body.style.backgroundImage = getRandomBackgroundImage();
 });
 
-button.addEventListener('click', function( ){
-  var emoj = emojii();
-  var fac = fact();
-  document.querySelector('.email').classList.remove('hidden')
-  document.querySelector('.next--fact').classList.remove('hidden')
-
-  document.querySelector('.factss-box').innerHTML = ''
-  document.querySelector('.factss-box').innerHTML += `<div>${emoj} ${fac} ${emoj} </div>`
-
-  document.querySelector('a').href =`mailto:EMAILADDRESS?subject=Welcome To Kitchen Facts!!!!!&body=${fac}. Wow!`
+button.addEventListener('click', () => {
+  let emoji = getRandom(emojis);
+  let fact = getRandom(theFacts);
+  removeClass([email, nextFact]);
+  factBox.innerHTML = '';
+  factBox.innerHTML += `<div>${emoji} ${fact} ${emoji} </div>`;
+  email.href = `mailto:alexmichaelfritz@gmail.com?subject=Welcome To Kitchen Facts!!!!!&body=${fact}. Wow!`;
 });
 
-
-
-document.querySelector('.next--fact').addEventListener("click", function() {
+nextFact.addEventListener("click", () => {
   location.reload()
-  document.querySelector('body').style.backgroundImage = getTheBackgroundImage();
-})
+});
